d2afa66a2c10846cd9185876eb97ec93c3cf15a3
Zuck Xu <ziangxuu@gmail.com>
2021-10-20 14:11:54 -0400

    init commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import Chat from "./chat";
+
+function App() {
+
+  
+  fetch('https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits')
+  .then(response => response.json())
+  .then(commits => console.log(commits[0].author.login));
+  
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
